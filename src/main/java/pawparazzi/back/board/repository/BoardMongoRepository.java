6ce7be058e55a2d34bbc3cfc654f1c8d146f44fb
geg222 <147246023+geg222@users.noreply.github.com>
2025-02-25 13:22:00 +0900

    [REFEAT] 회원탈퇴 시 몽고 DB 게시물 정보 삭제
    [REFEAT] 회원탈퇴 시 몽고 DB 게시물 정보도 삭제 되도록 수정

--- src/main/java/pawparazzi/back/board/repository/BoardMongoRepository.java
+++ src/main/java/pawparazzi/back/board/repository/BoardMongoRepository.java
@@ -3,8 +3,10 @@ package pawparazzi.back.board.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import pawparazzi.back.board.entity.BoardDocument;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BoardMongoRepository extends MongoRepository<BoardDocument, String> {
     Optional<BoardDocument> findByMysqlId(Long mysqlId);
+    void deleteByMysqlId(Long mysqlId);
 }
