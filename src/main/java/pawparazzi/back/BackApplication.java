5dcf191bf6b25fd89ed55fee43c1474c33808c7a
geg222 <147246023+geg222@users.noreply.github.com>
2025-03-12 21:54:02 +0900

    [FIX] backApplication .env 불러오기
    [FIX] backApplication .env 불러오기

--- src/main/java/pawparazzi/back/BackApplication.java
+++ src/main/java/pawparazzi/back/BackApplication.java
@@ -1,5 +1,6 @@
 package pawparazzi.back;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -8,6 +9,14 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class BackApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        dotenv.entries().forEach(entry -> {
+            if (System.getProperty(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
+
         SpringApplication.run(BackApplication.class, args);
     }
 }
