6a88ffa5a4a1c7e6aeadac2b0dd42ba5dd61a07d
geg222 <gl021414@naver.com>
2025-05-16 14:14:05 +0900

    fix: 스케줄링 수정
    서버를 재시작하자마자 , 24시간마다 한번씩

--- src/main/java/pawparazzi/back/story/scheduler/StoryScheduler.java
+++ src/main/java/pawparazzi/back/story/scheduler/StoryScheduler.java
@@ -11,7 +11,7 @@ public class StoryScheduler {
 
     private final StoryService storyService;
 
-    @Scheduled(initialDelay = 60_000, fixedRate = 86_400_000) // 1분 뒤부터 매 24시간마다 실행
+    @Scheduled(fixedRate = 86_400_000) // 24시간마다 실행
     public void runExpireOldStories() {
         storyService.expireOldStories();
     }
