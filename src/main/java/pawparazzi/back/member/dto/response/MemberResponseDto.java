19a9baf43c53f77c013dbaa332fa08723ac1efcc
geg222 <gl021414@naver.com>
2025-05-25 21:45:08 +0900

    feat: 사용자 목록 조회에 펫데이터 포함
    feat: 사용자 목록 조회에 펫데이터 포함

--- src/main/java/pawparazzi/back/member/dto/response/MemberResponseDto.java
+++ src/main/java/pawparazzi/back/member/dto/response/MemberResponseDto.java
@@ -3,11 +3,27 @@ package pawparazzi.back.member.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+import java.time.LocalDate;
+
 @Getter
 @AllArgsConstructor
+
 public class MemberResponseDto {
     private Long id;
     private String name;
     private String nickName;
     private String profileImage;
+    private List<PetDto> pets;
+
+    @Getter
+    @AllArgsConstructor
+    public static class PetDto {
+        private Long id;
+        private String name;
+        private String type;
+        private LocalDate birthDate;
+        private String petDetail;
+        private String petImg;
+    }
 }
